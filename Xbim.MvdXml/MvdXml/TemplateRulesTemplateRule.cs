e6977e050618322007da725993d6b5198f39dec2
Claudio Benghi <claudio.benghi@gmail.com>
2017-04-04 19:41:44 +0100

    Lib updated. Improved debugging on TemplateRulesTemplateRule.

--- Xbim.MvdXml/MvdXml/TemplateRulesTemplateRule.cs
+++ Xbim.MvdXml/MvdXml/TemplateRulesTemplateRule.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using log4net;
 using Xbim.MvdXml.DataManagement;
 
 // ReSharper disable once CheckNamespace
@@ -17,9 +19,19 @@ namespace Xbim.MvdXml
         {
             if (ret == null)
                 return false;
+
             var thisLevelVal = MvdPropertyRuleValue.BuildSql(Parameters, ret);
-            var v = ret.Select(thisLevelVal).Any();
-            return v;
+            try
+            {
+                var v = ret.Select(thisLevelVal).Any();
+                return v;
+            }
+            catch (Exception ex)
+            {
+                var log = LogManager.GetLogger("Xbim.MvdXml.TemplateRulesTemplateRule");
+                log.Error($"Problem in parameters field \"{Parameters}\" for templaterule (Description: \"{Description}\").", ex);
+                return false;
+            }
         }
     }
 }
